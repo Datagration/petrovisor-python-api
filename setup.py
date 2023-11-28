3ec46da5ff21784a6740fbfaa544e0f3d4de0d7d
Roman Manasipov <110097955+romanmanasipov@users.noreply.github.com>
2023-11-28 15:32:53 +0100

    PyPI automation test (#8)
    * - add __version__
    - simplify setup.py
    
    * - added python package publish to pypi workflow

--- setup.py
+++ setup.py
@@ -1,30 +1,5 @@
+#!/usr/bin/env python
 import setuptools
 
-# long description
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name="petrovisor",
-    version="0.1.2",
-    author="Datagration Solutions Inc.",
-    author_email="developers@datagration.com",
-    description="Python API for PetroVisor platform.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Datagration/petrovisor-python-api",
-    classifiers=[
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
-)
+if __name__ == "__main__":
+    setuptools.setup()
