d52880055209ff073657c40fa1f9e41fb2b4f03e
Roman Manasipov <rmanasipov@datagration.com>
2023-12-02 16:54:26 +0100

    added new entities and signals test

---
+++ tests/test_signals.py
@@ -0,0 +1,44 @@
+from petrovisor import PetroVisor
+import pandas as pd
+import numpy as np
+import pytest
+
+
+def test_signals_by_entity(pv_api: PetroVisor):
+    # add new entity
+    entity_name = r"_entity with special characters %*,$&^§()#//=2!~*'"  # except '?', ';'
+    pv_api.add_item('Entity', {
+        'Name': entity_name,
+        'EntityTypeName': 'Well',
+        'Alias': '',
+        'IsOpportunity': False,
+    })
+    # rename entity
+    new_entity_name = entity_name.replace('_entity', '_renamed entity')
+    # pv_api.rename_entity(old_name=entity_name, new_name=new_entity_name)
+    # delete entity
+    # pv_api.delete_entity(new_entity_name)
+    # add signal
+    signal_name = 'Time Signal'
+    signal_unit = ' '
+    max_length = 29
+    short_signal_name = signal_name[:max_length] if len(signal_name) > max_length else signal_name
+    pv_api.add_item('Signal', {
+        'Name': signal_name,
+        'ShortName': short_signal_name,
+        'SignalType': 'TimeDependent',
+        'MeasurementName': 'Dimensionless',
+        'StorageUnitName': signal_unit,
+        'AggregationType': 'Sum',
+        'ContainerAggregationType': 'Sum',
+    })
+    # add signal data
+    num_rows = 5
+    signal_col = f"{signal_name} [{signal_unit}]"
+    df = pd.DataFrame({'Entity': np.repeat(entity_name, num_rows),
+                       'Date': pd.date_range("2023-11-29", periods=num_rows, freq="D"),
+                       signal_col: np.random.rand(num_rows)})
+    pv_api.save_table_data(df)
+    # get signals by entity
+    entity_signals = pv_api.get_signals(entity=entity_name, signal_type='time')
+    assert signal_name in [s['Name'] for s in entity_signals]
