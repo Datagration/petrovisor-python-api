f07fb66e1e5a097dd7b46f594b5f0bfe7e9c2c34
Roman Manasipov <rmanasipov@datagration.com>
2025-02-19 22:09:45 +0100

    converted attributes to properties

--- src/petrovisor/api/enums/arguments.py
+++ src/petrovisor/api/enums/arguments.py
@@ -1,76 +1,262 @@
 from typing import NewType
 
 
+class ArgumentItemType:
+    @property
+    def BlobFile(self):
+        return NewType("BlobFileArg", str)
+
+    @property
+    def Calculation(self):
+        return NewType("CalculationArg", str)
+
+    @property
+    def ChartDefinition(self):
+        return NewType("ChartDefinitionArg", str)
+
+    @property
+    def CleansingCalculation(self):
+        return NewType("CleansingCalculationArg", str)
+
+    @property
+    def CleansingScript(self):
+        return NewType("CleansingScriptArg", str)
+
+    @property
+    def Context(self):
+        return NewType("ContextArg", str)
+
+    @property
+    def CustomWorkflowActivity(self):
+        return NewType("CustomWorkflowActivityArg", str)
+
+    @property
+    def Dashboard(self):
+        return NewType("DashboardArg", str)
+
+    @property
+    def DataConnection(self):
+        return NewType("DataConnectionArg", str)
+
+    @property
+    def DataGrid(self):
+        return NewType("DataGridArg", str)
+
+    @property
+    def DataIntegrationSession(self):
+        return NewType("DataIntegrationSessionArg", str)
+
+    @property
+    def DataIntegrationSet(self):
+        return NewType("DataIntegrationSetArg", str)
+
+    @property
+    def DataSource(self):
+        return NewType("DataSourceArg", str)
+
+    @property
+    def DCA(self):
+        return NewType("DCAArg", str)
+
+    @property
+    def Entity(self):
+        return NewType("EntityArg", str)
+
+    @property
+    def EntitySet(self):
+        return NewType("EntitySetArg", str)
+
+    @property
+    def EntityType(self):
+        return NewType("EntityTypeArg", str)
+
+    @property
+    def EventCalculation(self):
+        return NewType("EventCalculationArg", str)
+
+    @property
+    def EventSubscription(self):
+        return NewType("EventSubscriptionArg", str)
+
+    @property
+    def FilterDefinition(self):
+        return NewType("FilterDefinitionArg", str)
+
+    @property
+    def Hierarchy(self):
+        return NewType("HierarchyArg", str)
+
+    @property
+    def Label(self):
+        return NewType("LabelArg", str)
+
+    @property
+    def Message(self):
+        return NewType("MessageArg", str)
+
+    @property
+    def MLModel(self):
+        return NewType("MLModelArg", str)
+
+    @property
+    def PivotTable(self):
+        return NewType("PivotTableArg", str)
+
+    @property
+    def Plot(self):
+        return NewType("PlotArg", str)
+
+    @property
+    def PowerBIItem(self):
+        return NewType("PowerBIItemArg", str)
+
+    @property
+    def ProcessTemplate(self):
+        return NewType("ProcessTemplateArg", str)
+
+    @property
+    def PSharpScript(self):
+        return NewType("PSharpScriptArg", str)
+
+    @property
+    def PythonWorkflowActivity(self):
+        return NewType("PythonWorkflowActivityArg", str)
+
+    @property
+    def ReferenceTable(self):
+        return NewType("ReferenceTableArg", str)
+
+    @property
+    def RWorkflowActivity(self):
+        return NewType("RWorkflowActivityArg", str)
+
+    @property
+    def Scenario(self):
+        return NewType("ScenarioArg", str)
+
+    @property
+    def Scope(self):
+        return NewType("ScopeArg", str)
+
+    @property
+    def Signal(self):
+        return NewType("SignalArg", str)
+
+    @property
+    def Snapshot(self):
+        return NewType("SnapshotArg", str)
+
+    @property
+    def Tag(self):
+        return NewType("TagArg", str)
+
+    @property
+    def TagEntry(self):
+        return NewType("TagEntryArg", str)
+
+    @property
+    def Ticket(self):
+        return NewType("TicketArg", str)
+
+    @property
+    def TicketTimeInterval(self):
+        return NewType("TicketTimeIntervalArg", str)
+
+    @property
+    def Unit(self):
+        return NewType("UnitArg", str)
+
+    @property
+    def Unknown(self):
+        return NewType("UnknownArg", str)
+
+    @property
+    def User(self):
+        return NewType("UserArg", str)
+
+    @property
+    def UserGroup(self):
+        return NewType("UserGroupArg", str)
+
+    @property
+    def UserSetting(self):
+        return NewType("UserSettingArg", str)
+
+    @property
+    def WebWorkflowActivity(self):
+        return NewType("WebWorkflowActivityArg", str)
+
+    @property
+    def Workflow(self):
+        return NewType("WorkflowArg", str)
+
+    @property
+    def WorkflowSchedule(self):
+        return NewType("WorkflowScheduleArg", str)
+
+    @property
+    def Workspace(self):
+        return NewType("WorkspaceArg", str)
+
+    @property
+    def WorkspaceValue(self):
+        return NewType("WorkspaceValueArg", str)
+
+    @property
+    def WorkspacePackage(self):
+        return NewType("WorkspacePackageArg", str)
+
+
 class ArgumentType:
     """Registry of argument types"""
 
     # signals
-    StaticNumericSignal = NewType("StaticNumericSignalArg", str)
-    StaticStringSignal = NewType("StaticStringSignalArg", str)
-    TimeNumericSignal = NewType("TimeNumericSignalArg", str)
-    TimeStringSignal = NewType("TimeStringSignalArg", str)
-    DepthNumericSignal = NewType("DepthNumericSignalArg", str)
-    DepthStringSignal = NewType("DepthStringSignalArg", str)
-    PVTSignal = NewType("PVTSignalArg", str)
+    @property
+    def StaticNumericSignal(self):
+        return NewType("StaticNumericSignalArg", str)
 
-    # general types
-    StringValue = NewType("StringValueArg", str)
-    StringOptions = NewType("StringOptionsArg", str)
-    ItemName = NewType("ItemNameArg", ArgumentItemType)
-    ItemOptions = NewType("ItemOptionsArg", ArgumentItemType)
+    @property
+    def StaticStringSignal(self):
+        return NewType("StaticStringSignalArg", str)
+
+    @property
+    def TimeNumericSignal(self):
+        return NewType("TimeNumericSignalArg", str)
+
+    @property
+    def TimeStringSignal(self):
+        return NewType("TimeStringSignalArg", str)
+
+    @property
+    def DepthNumericSignal(self):
+        return NewType("DepthNumericSignalArg", str)
+
+    @property
+    def DepthStringSignal(self):
+        return NewType("DepthStringSignalArg", str)
+
+    @property
+    def PVTSignal(self):
+        return NewType("PVTSignalArg", str)
 
     # specific types
-    TagEntries = NewType("TagEntriesArg", str)
+    @property
+    def TagEntries(self):
+        return NewType("TagEntriesArg", str)
 
-class ArgumentItemType:
-    BlobFile = NewType("BlobFileArg", str)
-    Calculation = NewType("CalculationArg", str)
-    ChartDefinition = NewType("ChartDefinitionArg", str)
-    CleansingCalculation = NewType("CleansingCalculationArg", str)
-    CleansingScript = NewType("CleansingScriptArg", str)
-    Context = NewType("ContextArg", str)
-    CustomWorkflowActivity = NewType("CustomWorkflowActivityArg", str)
-    Dashboard = NewType("DashboardArg", str)
-    DataConnection = NewType("DataConnectionArg", str)
-    DataGrid = NewType("DataGridArg", str)
-    DataIntegrationSession = NewType("DataIntegrationSessionArg", str)
-    DataIntegrationSet = NewType("DataIntegrationSetArg", str)
-    DataSource = NewType("DataSourceArg", str)
-    DCA = NewType("DCAArg", str)
-    Entity = NewType("EntityArg", str)
-    EntitySet = NewType("EntitySetArg", str)
-    EntityType = NewType("EntityTypeArg", str)
-    EventCalculation = NewType("EventCalculationArg", str)
-    EventSubscription = NewType("EventSubscriptionArg", str)
-    FilterDefinition = NewType("FilterDefinitionArg", str)
-    Hierarchy = NewType("HierarchyArg", str)
-    Label = NewType("LabelArg", str)
-    Message = NewType("MessageArg", str)
-    MLModel = NewType("MLModelArg", str)
-    PivotTable = NewType("PivotTableArg", str)
-    Plot = NewType("PlotArg", str)
-    PowerBIItem = NewType("PowerBIItemArg", str)
-    ProcessTemplate = NewType("ProcessTemplateArg", str)
-    PSharpScript = NewType("PSharpScriptArg", str)
-    PythonWorkflowActivity = NewType("PythonWorkflowActivityArg", str)
-    ReferenceTable = NewType("ReferenceTableArg", str)
-    RWorkflowActivity = NewType("RWorkflowActivityArg", str)
-    Scenario = NewType("ScenarioArg", str)
-    Scope = NewType("ScopeArg", str)
-    Signal = NewType("SignalArg", str)
-    Snapshot = NewType("SnapshotArg", str)
-    Tag = NewType("TagArg", str)
-    TagEntry = NewType("TagEntryArg", str)
-    Ticket = NewType("TicketArg", str)
-    TicketTimeInterval = NewType("TicketTimeIntervalArg", str)
-    Unit = NewType("UnitArg", str)
-    Unknown = NewType("UnknownArg", str)
-    User = NewType("UserArg", str)
-    UserGroup = NewType("UserGroupArg", str)
-    UserSetting = NewType("UserSettingArg", str)
-    WebWorkflowActivity = NewType("WebWorkflowActivityArg", str)
-    Workflow = NewType("WorkflowArg", str)
-    WorkflowSchedule = NewType("WorkflowScheduleArg", str)
-    Workspace = NewType("WorkspaceArg", str)
-    WorkspaceValue = NewType("WorkspaceValueArg", str)
-    WorkspacePackage = NewType("WorkspacePackageArg", str)
+    # general types
+    @property
+    def StringValue(self):
+        return NewType("StringValueArg", str)
+
+    @property
+    def StringOptions(self):
+        return NewType("StringOptionsArg", str)
+
+    @property
+    def Item(self):
+        return NewType("ItemArg", ArgumentItemType)
+
+    @property
+    def ItemOptions(self):
+        return NewType("ItemOptionsArg", ArgumentItemType)
